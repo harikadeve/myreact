f564e26bae237445ab6628b21d1556d20172ff7b
Harika Mutyala <hmutyala@seven.com>
2024-03-13 20:46:42 +0530

    Episode 1

---
+++ app.js
@@ -0,0 +1,11 @@
+const parent = React.createElement(
+    "div", 
+    {id: "parent"}, 
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {id: "h1"}, 'H1 tag'),
+        React.createElement("h2", {id: "h2"}, 'H2 tag')
+    ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
